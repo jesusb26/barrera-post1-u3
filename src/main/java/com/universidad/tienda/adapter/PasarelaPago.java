02dba50c1bb9a71ca7cd01aa45b5815d7e271ef8
jesús david barrera montes <jesusdavidbm@ufps.edu.co>
2026-03-30 15:43:53 -0500

    feat(adapter): implementar patrón Adapter con PayPal y Stripe

---
+++ src/main/java/com/universidad/tienda/adapter/PasarelaPago.java
@@ -0,0 +1,6 @@
+package com.universidad.tienda.adapter;
+// Interfaz que el sistema espera — contrato interno
+public interface PasarelaPago {
+ boolean procesarPago(String moneda, double monto, String tokenCliente);
+ String obtenerNombre();
+}
